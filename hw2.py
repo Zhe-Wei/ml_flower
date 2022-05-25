f0e59612858be04a52b22e52bea253fac30a0217
Zhe-Wei <s1410432041@gms.nutc.edu.tw>
2022-05-25 19:49:50 +0800

    RandomChoice+水平翻轉+垂直翻轉+旋轉 中心剪裁

--- hw2.py
+++ hw2.py
@@ -59,9 +59,16 @@ data_transforms = {
             # ]),
             # # transforms.CenterCrop((480,480)),
             # transforms.CenterCrop((480,480)),
-            torchvision.transforms.RandomHorizontalFlip(p=0.5),
-            torchvision.transforms.RandomRotation(20),
-            transforms.Resize((224,224) ),
+            # torchvision.transforms.RandomHorizontalFlip(p=0.5),
+            # torchvision.transforms.RandomRotation(20),
+            # transforms.Resize((224,224) ),
+
+            transforms.RandomChoice([
+              transforms.RandomHorizontalFlip(p=0.5),
+              transforms.RandomVerticalFlip(p=0.5),
+              transforms.RandomRotation(180),
+            ]),
+            transforms.CenterCrop((480,480)),
 
             ########################################
             transforms.ToTensor(),
