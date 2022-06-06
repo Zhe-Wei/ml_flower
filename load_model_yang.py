c020a1de675217f0954649c868f35808059f254a
Zhe-Wei <s1410432041@gms.nutc.edu.tw>
2022-06-06 11:42:06 +0800

    Init.  copy file from load_model.py Need to write dataset class

---
+++ load_model_yang.py
@@ -0,0 +1,488 @@
+from __future__ import print_function, division
+
+import json
+import time
+import os
+import copy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.hub import load_state_dict_from_url
+import torchvision
+from torchvision import datasets, models, transforms
+import torch.utils.data 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import pretrainedmodels
+
+plt.ion()   # interactive mode
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+"""## 圖像轉換
+### 題目
+torchvision.transforms 提供了許多可靠的 API來讓使用者對圖像進行操作，請試著在 data_transforms 當中對訓練集進行轉換(圖像前處理)，當模型訓練到一定程度時，驗證看看使用該方法是否確實對模型準確率造成影響，然後試著解釋使用該轉換方法會對模型訓練產生什麼影響。
+
+* 至少嘗試使用 **五種** 不同的圖像轉換方法，並且找出最佳的方法組合。(使用方法數量為加分bonus的依據)
+* 須在報告中註明每一個方法 **在未使用時的準確率**、**使用後的準確率**，並 **說明該方法的目的** 及 **最終最佳組合的準確率**。
+
+### 說明
+請在註解區塊中寫入圖像轉換的方法。
+
+"""
+
+data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomChoice([
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomRotation(degrees=(0,30)),
+            ]),
+            transforms.Resize((224,224) ),
+
+            ########################################
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize((224,224) ),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+"""## 修改模型架構
+### 第一題 題目
+在本次作業範例中我們使用了CNN來做為整個分類模型的架構。請以第一題中最佳的圖像轉換方法組合，並基於CNN架構增加或減少模型的隱藏層，並觀察修改模型後對原先準確率的影響(即修改模型的意思)
+
+* 至少使用 **三種** 不同隱藏層或不同的修改模型方法(增加或減少模型的隱藏層，並且找出最佳的模型架構。(修改方法多寡為加分bonus的依據)
+* 須在報告中註明每一個方法 **在更改前的準確率**、 **更改後的準確率** 及 **最終最佳模型架構的準確率**。
+
+### 說明
+* 因為模型有套用預訓練的參數，所以更改模型的方式比較複雜，
+
+* 請勿直接更改現有隱藏層的參數(輸入、輸出大小等等)，請用增加或減少的方式來修改模型架構。
+
+* 請注意並計算各隱藏層可接受的輸出入大小，以免產生資料維度前後層對不上的問題。
+
+"""
+
+class MyCNN(nn.Module):
+
+  def __init__(self, num_classes=1000):
+    super(MyCNN, self).__init__()
+    self.features = nn.Sequential(
+      #============== 在此區塊新增或減少隱藏層 =================
+      nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2),
+
+      nn.Conv2d(64, 192, kernel_size=5, padding=2),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2),
+      
+      nn.Conv2d(192, 384, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(384, 256, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(256, 256, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2),
+      #==========================================================
+    )
+    self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+    self.classifier = nn.Sequential(
+      nn.Dropout(),
+      nn.Linear(256 * 6 * 6, 4096),#全連接層 fully connected
+      nn.ReLU(inplace=True),
+
+      nn.Dropout(),#避免過擬和
+      nn.Linear(4096, 4096),
+      nn.ReLU(inplace=True),
+      
+      # nn.Linear(4096, num_classes), # 原始模型輸出層
+    )
+
+    self.myLinear = nn.Linear(4096, num_classes)
+
+
+  def forward(self, x):
+    x = self.features(x)
+    x = self.avgpool(x)
+    x = torch.flatten(x, 1)
+    x = self.classifier(x)
+
+    x = self.myLinear(x)
+
+    return x
+
+"""## 訓練模型區塊
+包含視覺化模型及訓練模型。
+"""
+
+def visualize_model(model, device, dataloaders, class_names, num_images=6):
+  was_training = model.training
+  model.eval()
+  images_so_far = 0
+
+  plt.figure(figsize=(18,9))
+
+  with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloaders['val']):
+      inputs = inputs.to(device)
+      labels = labels.to(device)
+
+      outputs = model(inputs)
+      _, preds = torch.max(outputs, 1)
+
+      for j in range(inputs.size()[0]):
+        images_so_far += 1
+
+        img_display = np.transpose(inputs.cpu().data[j].numpy(), (1,2,0)) #numpy:CHW, PIL:HWC
+        plt.subplot(num_images//2,2,images_so_far),plt.imshow(img_display) #nrow,ncol,image_idx
+        plt.title(f'predicted: {class_names[preds[j]]}')
+        plt.savefig(os.path.join('./output/' + log_time, 'predict.jpg'))
+        if images_so_far == num_images:
+            model.train(mode=was_training)
+            return
+    model.train(mode=was_training)
+
+def imshow(inp, title=None):
+  """Imshow for Tensor."""
+  inp = inp.numpy().transpose((1, 2, 0))
+  mean = np.array([0.485, 0.456, 0.406])
+  std = np.array([0.229, 0.224, 0.225])
+  
+  #原先Normalize是對每個channel個別做 減去mean, 再除上std
+  inp1 = std * inp + mean
+
+  plt.imshow(inp)
+  # plt.savefig(f"./Normalize1.png")
+  os.mkdir('./output/'+log_time)
+  plt.savefig(os.path.join('./output/' + log_time, 'Normalize1.png'))
+  # if title is not None:
+  #     plt.title(title)
+  plt.pause(0.001)  # pause a bit so that plots are updated
+
+  plt.imshow(inp1)
+  plt.savefig(os.path.join('./output/' + log_time, 'non-Normalize.png'))
+  
+  # if title is not None:
+  #     plt.title(title)
+
+
+def count_parameters(model):
+  return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def train_model(model, criterion, device, dataloaders, dataset_sizes, optimizer, scheduler, num_epochs=25):
+  global model_name 
+  model_name = model.__class__.__name__
+  since = time.time()
+
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_acc = 0.0
+  train_loss, valid_loss = [], []
+  train_acc, valid_acc = [], []
+
+  for epoch in range(num_epochs):
+    print('Epoch {}/{}'.format(epoch+1, num_epochs))
+    print('-' * 10)
+
+    # Each epoch has a training and validation phase
+    for phase in ['train', 'val']:
+      if phase == 'train':
+        model.train()  # Set model to training mode
+      else:
+        model.eval()   # Set model to evaluate mode
+
+      running_loss = 0.0
+      running_corrects = 0
+
+      # Iterate over data.
+      for inputs, labels in tqdm(dataloaders[phase]):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == 'train'):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+
+            # backward + optimize only if in training phase
+            if phase == 'train':
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+      if phase == 'train':
+        scheduler.step()
+
+      epoch_loss = running_loss / dataset_sizes[phase]
+      epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+      if phase == 'train':
+        train_loss.append(epoch_loss)
+        train_acc.append(epoch_acc)
+      else:
+        valid_loss.append(epoch_loss)
+        valid_acc.append(epoch_acc)
+
+      print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+        phase, epoch_loss, epoch_acc))
+
+      # deep copy the model
+      if phase == 'val' and epoch_acc > best_acc:
+        best_acc = epoch_acc
+        best_model_wts = copy.deepcopy(model.state_dict())
+
+
+  plt.figure()
+  plt.plot(range(1,num_epochs+1,1), np.array(train_loss), 'r-', label= "train loss") #relative global step
+  # plt.xlabel('epoch')
+  # plt.ylabel('loss')
+  # plt.legend()
+  # plt.savefig(f"./train_loss.png")
+
+  # plt.figure(1)
+  plt.plot(range(1,num_epochs+1,1), np.array(valid_loss), 'b-', label= "eval loss") #--evaluate_during_training True 在啟用eval
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.legend()
+  plt.savefig(os.path.join('./output/' + log_time, './eval_loss.png'))
+  
+  time_elapsed = time.time() - since
+  print('Training complete in {:.0f}m {:.0f}s'.format(
+    time_elapsed // 60, time_elapsed % 60))
+  print('Best val Acc: {:4f}'.format(best_acc))
+
+  print(valid_acc[0].item())
+
+  output_dict = {
+    # "cross_number": int(data_dir[-1]),
+    "num_epochs": num_epochs,
+    "lr": lr,
+    "batch_size": batch_size,
+    "train_loss": (train_loss),
+    "train_acc": (train_acc[0].item()),
+    "valid_loss": (valid_loss),
+    "valid_acc": (valid_acc[0].item()),
+    "best_acc": (best_acc.item()),
+    "time_elapsed(min)": time_elapsed // 60,
+    "time_elapsed(s)": time_elapsed % 60,
+    # "optimizer": type(optimizer),
+    "model_name": model_name,
+    "model_name_sub": "densenet169"
+    # "#parameters": parameter_count
+  }
+  print(output_dict)
+  json_object = json.dumps(output_dict, indent = 4)
+  filename = log_folder + model_name + '_' + log_time + '.json' 
+  print(filename)
+
+  with open(filename, "w") as outfile:
+    outfile.write(json_object)
+  # load best model weights
+  model.load_state_dict(best_model_wts)
+  torch.save(model.state_dict(),"./output/model.pt")
+  return model
+
+def test_model(model, device, dataloaders, class_names):
+
+  model.load_state_dict(torch.load('./output/model.pt'))
+  model.eval()
+  print(model)
+  return 
+  # was_training = model.training
+  images_so_far = 0
+
+  # plt.figure(figsize=(18,9))
+
+  with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloaders['val']):
+      inputs = inputs.to(device)
+      labels = labels.to(device)
+
+      outputs = model(inputs)
+      _, preds = torch.max(outputs, 1)
+
+      # for j in range(inputs.size()[0]):
+      #   images_so_far += 1
+
+      #   img_display = np.transpose(inputs.cpu().data[j].numpy(), (1,2,0)) #numpy:CHW, PIL:HWC
+      #   plt.subplot(num_images//2,2,images_so_far),plt.imshow(img_display) #nrow,ncol,image_idx
+      #   plt.title(f'predicted: {class_names[preds[j]]}')
+      #   plt.savefig(os.path.join('./output/' + log_time, 'predict.jpg'))
+      #   if images_so_far == num_images:
+      #       model.train(mode=was_training)
+      #       return
+    # model.train(mode=was_training)
+  print(predict)
+"""## 訓練參數 (可調整)
+* num_epochs: 訓練回合數
+* lr: 訓練速度(learning rate)
+* batch_size: 批次(batch)大小
+"""
+model_name = ''
+log_folder = './output/'
+log_time = ''
+data_dir = ''
+
+num_epochs = 1
+lr = 0.0025
+batch_size = 8
+
+"""## 主函式"""
+
+def main():
+  global batch_size 
+  global log_time
+  global data_dir
+  global lr
+
+  # data_dir = '../Cross-validation/Cross-validation-1'
+  data_dir = './training'
+  print(data_dir)
+  
+  log_time = time.strftime("%Y%m%d_%H:%M:%S.%f")[:-3]
+  log_time = log_time + '_bs_' + str(batch_size)
+  num_workers = 2
+  momentum = 0.9
+
+  # 資料集載入 =======================================================================
+  class ExampleDataset(torch.utils.data.Dataset):
+    def __init__(self):
+      self.data = "abcdefghijklmnopqrstuvwxyz"
+    
+    def __getitem__(self,idx): # if the index is idx, what will be the data?
+      return self.data[idx]
+    
+    def __len__(self): # What is the length of the dataset
+      return len(self.data)
+
+  dataset1 = ExampleDataset() # create the dataset
+  dataloader = torch.utils.data.DataLoader(
+                        dataset = dataset1, 
+                        shuffle = True, 
+                        batch_size = 1
+               )
+
+  image_datasets = {
+    x: datasets.ImageFolder(
+      os.path.join(data_dir, x),
+      data_transforms[x]
+    ) 
+    for x in ['train', 'val']
+  }
+  dataloaders = {
+    x: torch.utils.data.DataLoader(
+      image_datasets[x], 
+      batch_size=batch_size,
+      shuffle=True, 
+      num_workers=num_workers
+    )
+    for x in ['train', 'val']
+  }
+  dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+  class_names = image_datasets['train'].classes
+  # 資料集載入 =======================================================================
+
+  # 設定 CUDA 環境 =======================================================================
+  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+  print(f"Using device {device}\n")
+
+  # 設定 CUDA 環境 =======================================================================
+
+
+  # Get a batch of training data
+  inputs, classes = next(iter(dataloaders['train']))
+
+  # Make a grid from batch
+  out = torchvision.utils.make_grid(inputs)
+
+  imshow(out, title=[class_names[x] for x in classes])
+
+  
+  # model =======================================================================
+  # model_ft = MyCNN(num_classes=219)
+
+  
+  # model_ft = models.googlenet(pretrained=True)
+
+  # model_ft = pretrainedmodels.__dict__['resnext101_32x4d'](num_classes=1000, pretrained='imagenet')
+  # print(model_ft)
+
+  # model_ft = torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x16d_wsl')
+
+  # model_ft = pretrainedmodels.__dict__['se_resnet101'](num_classes=1000, pretrained='imagenet')
+
+  # num_ftrs = model_ft.last_linear.in_features
+  # model_ft.last_linear = nn.Linear(num_ftrs, 219)
+
+  model_ft = torch.hub.load('pytorch/vision:v0.10.0', 'densenet169', pretrained=True)
+  num_ftrs = model_ft.classifier.in_features
+  model_ft.classifier = nn.Linear(num_ftrs,219)
+
+  # num_ftrs = model_ft.fc.in_features
+  # model_ft.fc = nn.Linear(num_ftrs, 219)
+
+  # num_ftrs = model_ft.classifier[2].in_features
+  # model_ft.classifier[3] = nn.Linear(num_ftrs,219)
+  
+  print(model_ft)
+  # pretrained_dict = load_state_dict_from_url(
+  #   'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+  #   progress=True
+  # )
+  # model_dict = model_ft.state_dict()
+  # # 1. filter out unnecessary keys
+  # pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+  # # 2. overwrite entries in the existing state dict
+  # model_dict.update(pretrained_dict) 
+  # # 3. load the new state dict
+  # model_ft.load_state_dict(model_dict)
+
+  # for k,v in model_dict.items():
+  #   print(k)
+
+  model_ft = model_ft.to(device)
+  # model =======================================================================
+
+  parameter_count = count_parameters(model_ft)
+  print(f"#parameters:{parameter_count}")
+  print(f"batch_size:{batch_size}")
+
+
+  criterion = nn.CrossEntropyLoss()
+
+  # Observe that all parameters are being optimized
+  optimizer_ft = optim.SGD(model_ft.parameters(), lr=lr, momentum=momentum)
+
+  # Decay LR by a factor of 0.1 every 7 epochs
+  exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+  # model_ft = train_model(
+  #   model_ft, 
+  #   criterion, 
+  #   device, 
+  #   dataloaders, 
+  #   dataset_sizes, 
+  #   optimizer_ft, 
+  #   exp_lr_scheduler,     
+  #   num_epochs=num_epochs
+  # )
+
+  # visualize_model(model_ft, device, dataloaders, class_names)
+
+  test_model(model_ft, device, dataloaders, class_names)
+
+if __name__ == '__main__':
+    main()
